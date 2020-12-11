71410cc5c3457f830a7eea949efd7af7e726e603
MUSgitudev <72415650+MUSgitudev@users.noreply.github.com>
2020-12-11 14:48:18 -0500

    made view model

---
+++ Muhammad_TaskMe WatchKit Extension/view_model/TaskViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  TaskViewModel.swift
+//  Muhammad_TaskMe WatchKit Extension
+//
+//  Created by MUS on 2020-12-11.
+//
+
+import Foundation
+
+class TaskViewModel : ObservableObject{
+    @Published var taskList = [Task]()
+    @Published var boredURLString: String = #"http://www.boredapi.com/api/activity"#
+
+    func fetchDataFromAPI(lat: Double, lon: Double){
+        
+        //boredURLString = String(format: boredURLString, lat, lon, self.access_id, self.secret_key)
+        
+        print(#function, "url is \(boredURLString)")
+        
+        guard let apiURL = URL(string: boredURLString) else{
+            return
+        }
+        
+        URLSession.shared.dataTask(with: apiURL){
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let er = error{
+                print(#function, "Error \(er.localizedDescription)")
+            }else{
+
+                DispatchQueue.global().async {
+                    do{
+                        if let jsonData = data{
+
+                            
+                            let decodedResponse = try JSONDecoder().decode(Task.self, from: jsonData)
+                            
+                            let decodedList : [Task] = [decodedResponse]
+                            
+                            DispatchQueue.main.async {
+                                self.taskList = decodedList
+                                print(#function, self.taskList)
+                            }
+                            
+                            
+                        }else{
+                            print(#function, "Empty JSON data :(")
+                        }
+                    }catch let error{
+                        print(#function, "Error decoding the data \(error)")
+                    }
+                }
+            }
+            
+        }.resume()
+    }
+}
