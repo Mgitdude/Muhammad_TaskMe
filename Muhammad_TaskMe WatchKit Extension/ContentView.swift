a8398ccca66c7fb20aa1104569b2ba7a51cae4f7
MUSgitudev <72415650+MUSgitudev@users.noreply.github.com>
2020-12-11 14:26:00 -0500

    Initial Commit

---
+++ Muhammad_TaskMe WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Muhammad_TaskMe WatchKit Extension
+//
+//  Created by MUS on 2020-12-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Muhammad_TaskMe WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  Muhammad_TaskMe WatchKit Extension
+//
+//  Created by MUS on 2020-12-11.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
